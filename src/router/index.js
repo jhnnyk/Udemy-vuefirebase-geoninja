03262f815d9e59a77d097c1f66c6fc7b350f3bdc
John Knotts <jhnnyk@gmail.com>
2019-11-14 21:22:03 -0700

    creating a signup page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import GMap from '@/views/GMap';
+import Signup from '@/views/Signup';
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'GMap',
     component: GMap
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup
   }
 ];
 

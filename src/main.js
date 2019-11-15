fb5e08b8bce967ddd1da3dea2e8c200e07cc2a12
John Knotts <jhnnyk@gmail.com>
2019-11-15 16:50:33 -0700

    wrapping the vue instance

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+let app = null;
+
+// wait for firebase auth to init before creating the app
+firebase.auth().onAuthStateChanged(() => {
+  // init app if not already created
+  if (!app) {
+    app = new Vue({
+      router,
+      render: h => h(App)
+    }).$mount('#app');
+  }
+});

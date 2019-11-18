52b2b6925968a5366aa5f07acdd07c03e953eae3
John Knotts <jhnnyk@gmail.com>
2019-11-18 07:20:46 -0700

    calling a cloud function

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -27,6 +27,7 @@ import slugify from 'slugify';
 import db from '@/firebase/init';
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/functions';
 
 export default {
   name: 'Signup',
@@ -47,20 +48,22 @@ export default {
           remove: /[$*_+~.()'"!\-:@]/g,
           lower: true
         });
-        let ref = db.collection('users').doc(this.slug);
-        ref.get().then(doc => {
-          if (doc.exists) {
+        let checkAlias = firebase.functions().httpsCallable('checkAlias');
+        checkAlias({ slug: this.slug }).then(result => {
+          if (!result.data.unique) {
             this.feedback = 'This alias already exists';
           } else {
             firebase
               .auth()
               .createUserWithEmailAndPassword(this.email, this.password)
               .then(cred => {
-                ref.set({
-                  alias: this.alias,
-                  geolocation: null,
-                  user_id: cred.user.uid
-                });
+                db.collection('users')
+                  .doc(this.slug)
+                  .set({
+                    alias: this.alias,
+                    geolocation: null,
+                    user_id: cred.user.uid
+                  });
               })
               .then(() => {
                 this.$router.push({ name: 'GMap' });
@@ -72,7 +75,6 @@ export default {
             this.feedback = 'This alias is free to use';
           }
         });
-        console.log(this.slug);
       } else {
         this.feedback = 'You must enter all fields';
       }
